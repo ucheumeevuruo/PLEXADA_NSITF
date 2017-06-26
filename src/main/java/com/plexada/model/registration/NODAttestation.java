55760576f26b40a468ef0b2cfa68a00622a1b096
SAP Training <SAP Training@SAPTRAINING-PC>
2017-06-26 13:25:12 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/com/plexada/controller/FormController.java

---
+++ src/main/java/com/plexada/model/registration/NODAttestation.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.plexada.model.registration;
+
+/**
+ *
+ * @author SAP Training
+ */
+public class NODAttestation {
+    private String Signature;
+    private String Stamp;
+    private String name;
+    private String div_checkbox;
+
+    /**
+     * @return the Signature
+     */
+    public String getSignature() {
+        return Signature;
+    }
+
+    /**
+     * @param Signature the Signature to set
+     */
+    public void setSignature(String Signature) {
+        this.Signature = Signature;
+    }
+
+    /**
+     * @return the Stamp
+     */
+    public String getStamp() {
+        return Stamp;
+    }
+
+    /**
+     * @param Stamp the Stamp to set
+     */
+    public void setStamp(String Stamp) {
+        this.Stamp = Stamp;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the div_checkbox
+     */
+    public String getDiv_checkbox() {
+        return div_checkbox;
+    }
+
+    /**
+     * @param div_checkbox the div_checkbox to set
+     */
+    public void setDiv_checkbox(String div_checkbox) {
+        this.div_checkbox = div_checkbox;
+    }
+   
+    
+}
---
+++ src/main/java/com/plexada/model/registration/NoAAttestation.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.plexada.model.ecs;
+
+/**
+ *
+ * @author SAP Training
+ */
+public class NoAAttestation {
+    private String Signature;
+    private String Stamp;
+    private String name;
+    private String div_checkbox;
+    
+}
